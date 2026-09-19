56859525a628578eb67d3dc25853dc1cc763afaf
CatKhakis <violet@funkycat.media>
2026-09-19 17:23:03 -0500

    update files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { randomUUID } = require('node:crypto');
 
 const { DatabaseSync } = require('node:sqlite');
-const database = new DatabaseSync('db/hormone.db');
+const database = new DatabaseSync('./db/hormone.db');
 
 const app = express();
 const port = 3000;
