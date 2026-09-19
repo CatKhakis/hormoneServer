787d86a2033cd1bfa2053a4b3ec8c71d95fac7ea
CatKhakis <violet@funkycat.media>
2026-09-19 14:54:01 -0500

    implemented tests table

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 ### To-do
 - [x] allow multiple filters for get
 - [x] delete entry
-- [ ] generalize functions to work for test results
+- [x] generalize functions to work for test results
 - [x] return added entry
+- [ ] return updated entry
 - [x] put requests for edits
 - [x] change post request from body to query
--- index.js
+++ index.js
@@ -11,144 +11,10 @@ app.use(express.json());
 
 
 
-// Define a route for GET requests
-app.get('/injections', (req, res) => {
-
-    var query = 'SELECT * FROM injections';
-    var filters = [];
-
-    for(const item in req.query) {
-
-        if (keyArray.includes(item)) {
-
-            filters.push(item);
-        }
-    }
-
-    if (filters.length > 0) {
-        query = `${query} WHERE`
-
-        for(const filter in filters) {
-
-            query = `${query} ${filters[filter]} = '${req.query[filters[filter]].match(/[A-Za-z0-9.-]+/m)}'`
-
-            if (filter <= filters.length - 2) {
-                query = `${query} AND`
-            }
-        }
-    }
-
-    const selectQuery = database.prepare(query);
-    res.json({ message: selectQuery.all()});
-});
-
-
-
-// Define a route for POST requests
-app.post('/injections', (req, res) => {
-
-    var values;
-    const uuid = crypto.randomUUID()
-
-    for(const item of keyArray) {
-
-        if (item == 'uuid') {
-            values = `'${uuid}'`
-        } else {
-
-            if (typeof req.query[item] == 'string') {
-                values = `${values}, '${req.query[item].match(/[A-Za-z0-9.-]+/m)}'`
-            } else if (typeof req.query[item] == 'number') {
-                values = `${values}, ${req.query[item]}`
-            } else {
-                values = `${values}, NULL`
-            }
-        }
-    }
-
-    const insert = database.prepare(`INSERT INTO injections (uuid, time, ester, concentration, dose, site, recipient, vial, notes) VALUES (${values})`);
-    
-    try {
-
-        insert.all();
-        res.json({ result: database.prepare(`SELECT * FROM injections WHERE uuid = '${uuid}'`).all()});
-
-    } catch(err) {
-        console.warn(err);
-        res.json({ message: err});
-    }
-});
-
-
-
-// Define a route for PUT requests
-app.put('/injections', (req, res) => {
-
-
-    if(req.query.uuid) {
-
-        const uuid = req.query.uuid.match(/[A-Za-z0-9.-]+/m);
-        var query = 'UPDATE injections\nSET';
-
-        var filters = [];
-
-        for(const item in req.query) {
-
-            if (keyArray.includes(item) && item != 'uuid') {
-
-                filters.push(item);
-            }
-        }
-
-        if (filters.length > 0) {
-
-            for(const filter in filters) {
-
-                query = `${query} ${filters[filter]} = '${req.query[filters[filter]].match(/[A-Za-z0-9.-]+/m)}'`
-
-                if (filter <= filters.length - 2) {
-                    query = `${query},\n`
-                }
-            }
-        }
-
-        query = `${query}\nWHERE uuid = '${uuid}'`
-
-        try {
-
-            const selectQuery = database.prepare(query);
-            res.json({ message: selectQuery.all()});
-
-        } catch(err) {
-            console.warn(err);
-            res.json({ message: err});
-        }
-        
-    } else {
-        res.json({ message: 'error: no uuid provided'});
-    }
-});
-
-
-
-// Define a route for GET requests
-app.delete('/injections', (req, res) => {
-    const query = database.prepare(`UPDATE FROM injections where uuid = '${req.query.uuid.match(/[A-Za-z0-9.-]+/m)}'`);
-    query.all();
-
-    res.json({ message: 'row deleted', uuid: req.query.uuid});
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
-
-
 const tables = {
     'injections': `
         CREATE TABLE injections(
-            uuid TEXT PRIMARY KEY,
+            uuid TEXT UNIQUE PRIMARY KEY,
             time INTEGER NOT NULL,
             ester TEXT NOT NULL,
             concentration REAL NOT NULL,
@@ -159,15 +25,25 @@ const tables = {
             notes TEXT
         ) STRICT
         `,
-    // 'tests': `
-    //     CREATE TABLE tests(
-    //         key INTEGER PRIMARY KEY,
-    //         test TEXT
-    //     ) STRICT
-    //     `,
+    'tests': `
+        CREATE TABLE tests(
+            uuid TEXT UNIQUE PRIMARY KEY,
+            time INTEGER NOT NULL,
+            test TEXT NOT NULL,
+            unit TEXT NOT NULL,
+            value REAL NOT NULL,
+            recipient TEXT,
+            notes TEXT
+        ) STRICT
+        `,
 };
 
-const keyArray = tables['injections'].match(/([a-z]+)(?= )/gm)
+const keyArray = [];
+
+for (const table in tables) {
+
+    keyArray[table] = tables[`${table}`].match(/([a-z]+)(?= )/gm)
+}
 
 for(const item in tables) {
 
@@ -179,4 +55,150 @@ for(const item in tables) {
         console.log(`table "${item}" exists!`)
     }
 }
+
+
+
+// Define a route for GET requests
+for (const table in tables) {
+
+    app.get(`/${table}`, (req, res) => {
+
+        var query = `SELECT * FROM ${table}`;
+        var filters = [];
+
+        for(const item in req.query) {
+
+            if (keyArray[table].includes(item)) {
+
+                filters.push(item);
+            }
+        }
+
+        if (filters.length > 0) {
+            query = `${query} WHERE`
+
+            for(const filter in filters) {
+
+                query = `${query} ${filters[filter]} = '${req.query[filters[filter]].match(/[A-Za-z0-9.-]+/m)}'`
+
+                if (filter <= filters.length - 2) {
+                    query = `${query} AND`
+                }
+            }
+        }
+
+        const selectQuery = database.prepare(query);
+        res.json({ message: selectQuery.all()});
+    });
+}
+
+
+
+// Define a route for POST requests
+for (const table in tables) {
+
+    app.post(`/${table}`, (req, res) => {
+
+        var values;
+        const uuid = crypto.randomUUID()
+
+        for(const item of keyArray[table]) {
+
+            if (item == 'uuid') {
+                values = `'${uuid}'`
+            } else {
+
+                if (typeof req.query[item] == 'string') {
+                    values = `${values}, '${req.query[item].match(/[A-Za-z0-9.-]+/m)}'`
+                } else if (typeof req.query[item] == 'number') {
+                    values = `${values}, ${req.query[item]}`
+                } else {
+                    values = `${values}, NULL`
+                }
+            }
+        }
+
+        const insert = database.prepare(`INSERT INTO ${table} (${keyArray[table].toString()}) VALUES (${values})`);
+        
+        try {
+
+            insert.all();
+            res.json({ result: database.prepare(`SELECT * FROM ${table} WHERE uuid = '${uuid}'`).all()});
+
+        } catch(err) {
+            console.warn(err);
+            res.json({ message: err});
+        }
+    });
+}
+
+
+
+// Define a route for PUT requests
+for (const table in tables) {
+
+    app.put(`/${table}`, (req, res) => {
+
+
+        if(req.query.uuid) {
+
+            const uuid = req.query.uuid.match(/[A-Za-z0-9.-]+/m);
+            var query = `UPDATE ${table}\nSET`;
+
+            var filters = [];
+
+            for(const item in req.query) {
+
+                if (keyArray[table].includes(item) && item != 'uuid') {
+
+                    filters.push(item);
+                }
+            }
+
+            if (filters.length > 0) {
+
+                for(const filter in filters) {
+
+                    query = `${query} ${filters[filter]} = '${req.query[filters[filter]].match(/[A-Za-z0-9.-]+/m)}'`
+
+                    if (filter <= filters.length - 2) {
+                        query = `${query},\n`
+                    }
+                }
+            }
+
+            query = `${query}\nWHERE uuid = '${uuid}'`
+
+            try {
+
+                const selectQuery = database.prepare(query);
+                res.json({ message: selectQuery.all()});
+
+            } catch(err) {
+                console.warn(err);
+                res.json({ message: err});
+            }
+            
+        } else {
+            res.json({ message: 'error: no uuid provided'});
+        }
+    });
+}
+
+
+
+// Define a route for GET requests
+for (const table in tables) {
+    app.delete(`/${table}`, (req, res) => {
+        const query = database.prepare(`UPDATE FROM ${table} where uuid = '${req.query.uuid.match(/[A-Za-z0-9.-]+/m)}'`);
+        query.all();
+
+        res.json({ message: 'row deleted', uuid: req.query.uuid});
+    });
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
 
